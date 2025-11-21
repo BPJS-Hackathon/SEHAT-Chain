dab62e3109e47cbd1526adf63c1ca9b063d32310
theerlant <ttrinanda2@gmail.com>
2025-11-21 12:56:36 +0800

    refactor: forgot i moved certificate

--- internal/consensus/certificate.go
+++
@@ -1,6 +0,0 @@
-package consensus
-
-type QuorumCertificate struct {
-	HeaderHash []byte            `json:"header_hash"`
-	Signatures map[string]string `json:"signatures"` // map signature berdasarkan id
-}
