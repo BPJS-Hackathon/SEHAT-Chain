55b0a127c72ca5dbdc57b5e732303b513ffc2632
theerlant <ttrinanda2@gmail.com>
2025-11-21 12:55:44 +0800

    feat: mocking credential

--- utils/crypto.go
+++ utils/crypto.go
@@ -1,2 +1,24 @@
-// Hashing, UUID Gen, Simple Signature check
 package utils
+
+import "fmt"
+
+// Mock keypair
+type CryptoCred struct {
+	secret string
+}
+
+func NewCred(secret string) *CryptoCred {
+	return &CryptoCred{
+		secret: secret,
+	}
+}
+
+func (cc *CryptoCred) Sign(data []byte) string {
+	return cc.secret
+}
+
+func (cc *CryptoCred) Validate(signature string, data []byte) error {
+	if signature != cc.secret {
+		return fmt.Errorf("wrong signature, has (%s) but calculated (%s)", signature, cc.secret)
+	}
+}
